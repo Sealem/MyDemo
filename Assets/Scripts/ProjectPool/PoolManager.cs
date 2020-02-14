fc30b4e3caf6861388d84d0d87cdb56049ebe4ef
Sealem <lei1289vip@qq.com>
2020-02-14 18:23:50 +0800

    first commit

---
+++ Assets/Scripts/ProjectPool/PoolManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    private List<GameObject> gameObjects;
+    public static PoolManager Instance;
+    public int PoolMax = 10;
+
+    public GameObject prefab;
+
+    void Awake()
+    {
+        Instance = this;
+        gameObjects = new List<GameObject>();
+    }
+
+    public GameObject GetGameObject()
+    {
+        if (gameObjects.Count <= 0)
+        {
+            return Instantiate(prefab);
+        }
+        GameObject localObj = gameObjects[0];
+        gameObjects.RemoveAt(0);
+        return localObj;
+    }
+
+    public void SetGameObject(GameObject obj)
+    {
+        if (gameObjects.Count >= PoolMax)
+        {
+            
+        }
+    }
+
+}
---
+++ Assets/Scripts/UIMain/UIMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMain : MonoBehaviour
+{
+    [SerializeField]
+    public MainButton[] MainButtons = new MainButton[0];
+
+    void Start()
+    {
+        for (int i = 0; i < MainButtons.Length; i++)
+        {
+            OnButtonClick(MainButtons[i]);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnButtonClick(MainButton btn)
+    {
+        
+        btn.ButtonName.onClick.AddListener(()=>{ Instantiate(btn.value); });
+    }
+
+}
+
+[Serializable]
+public class MainButton
+{
+    public Button ButtonName;
+    public GameObject value;
+    public int a;
+}
